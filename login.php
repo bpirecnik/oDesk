73e665d1959787a65c3098fd30fb60e30bfd6d82
Boris Pirečnik <Boris Pirenik>
2017-06-11 12:32:29 +0200

    Added roles to database

--- login.php
+++ login.php
@@ -2,6 +2,17 @@
     include_once 'header.php';
 ?>
 
+<?php
+    $pass = ('93487gh59874ldsk439875admin');
+    echo "<p>Geslo: ".$pass."</p>";
+?>
+
+
+<?php
+    $pass = ('93487gh59874ldsk439875admin');
+    echo "Geslo: ".sha1($pass);
+?>
+
 <h1>Prijava</h1>
 
 <form action="login_check.php" method="POST">
