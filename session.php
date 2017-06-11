ec0b09a2226433645c2aa60a4abc8c267e01bf44
Boris Pirečnik <Boris Pirenik>
2017-06-11 11:39:05 +0200

    Editing the coe

--- session.php
+++ session.php
@@ -5,10 +5,10 @@
     //če ni, ga preusmerim na prijavo
     //izključim strani, potrebne za prvi obisk
     if (!isset($_SESSION['user_id']) &&
-            ($_SERVER['REQUEST_URI'] != '/odesk/login.php') &&
-            ($_SERVER['REQUEST_URI'] != '/odesk/user_add.php') &&
-            ($_SERVER['REQUEST_URI'] != '/odesk/login_check.php') &&
-            ($_SERVER['REQUEST_URI'] != '/odesk/index.php')) {
+            ($_SERVER['REQUEST_URI'] != '/Projekt_oDesk/login.php') &&
+            ($_SERVER['REQUEST_URI'] != '/Projekt_oDesk/user_add.php') &&
+            ($_SERVER['REQUEST_URI'] != '/Projekt_oDesk/login_check.php') &&
+            ($_SERVER['REQUEST_URI'] != '/Projekt_oDesk/index.php')) {
         header("Location: login.php");
     }
 ?>
