9a58342efd87fa2a48a362e26d74ff3ad2876f40
bpirecnik <pirecnikboris@gmail.com>
2017-06-15 03:30:44 +0200

    Last updates on database

---
+++ dokumenti/odesk_export.sql
@@ -0,0 +1,538 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 15, 2017 at 03:29 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `odesk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attachments`
+--
+
+CREATE TABLE `attachments` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `description` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attachments_messages`
+--
+
+CREATE TABLE `attachments_messages` (
+  `id` int(11) NOT NULL,
+  `attachment_id` int(11) NOT NULL,
+  `message_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `short` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `countries`
+--
+
+INSERT INTO `countries` (`id`, `title`, `short`) VALUES
+(1, 'Slovenia', 'SLO'),
+(2, 'United States', 'US');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `description` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `userfrom_id` int(11) NOT NULL,
+  `userto_id` int(11) NOT NULL,
+  `seen` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `price` float NOT NULL,
+  `start_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `end_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `description` text NOT NULL,
+  `stage` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`id`, `title`, `price`, `start_date`, `end_date`, `description`, `stage`) VALUES
+(1, 'Project oDesk', 1200, '2017-06-11 13:45:12', '2018-01-17 22:59:59', 'Creating web page with already established layout.', 1),
+(2, 'Project Lex', 120, '2017-06-13 12:05:53', '2017-06-29 21:59:59', 'Testing the multiple projects on one page on profile', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects_users`
+--
+
+CREATE TABLE `projects_users` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `skill_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `projects_users`
+--
+
+INSERT INTO `projects_users` (`id`, `role_id`, `user_id`, `project_id`, `skill_id`) VALUES
+(16, 1, 1, 1, 3),
+(17, 1, 3, 2, 4),
+(18, 2, 2, 2, 5),
+(19, 2, 1, 2, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratings`
+--
+
+CREATE TABLE `ratings` (
+  `id` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratings_users`
+--
+
+CREATE TABLE `ratings_users` (
+  `id` int(11) NOT NULL,
+  `usergiver_id` int(11) NOT NULL,
+  `usergetr_id` int(11) NOT NULL,
+  `rating1_id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `description` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `title`, `description`) VALUES
+(1, 'Creator', 'Creator of the project'),
+(2, 'Applicant', 'User that apply for the project'),
+(3, 'Worker', 'User that works/worked on the project');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills`
+--
+
+CREATE TABLE `skills` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `skills`
+--
+
+INSERT INTO `skills` (`id`, `category_id`, `title`, `description`) VALUES
+(3, 1, 'HTML', NULL),
+(4, 1, 'CSS', NULL),
+(5, 1, 'PHP', NULL),
+(6, 1, 'JavaScript', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills_users`
+--
+
+CREATE TABLE `skills_users` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `skill_id` int(11) NOT NULL,
+  `rank_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `skills_users`
+--
+
+INSERT INTO `skills_users` (`id`, `user_id`, `skill_id`, `rank_id`) VALUES
+(3, 1, 3, 2),
+(4, 1, 4, 2),
+(5, 1, 5, 3),
+(6, 1, 6, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `first_name` varchar(200) DEFAULT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `pass` varchar(100) NOT NULL,
+  `date_birth` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `description` text,
+  `avatar` varchar(255) DEFAULT NULL,
+  `hashcode` varchar(100) NOT NULL,
+  `active` int(11) NOT NULL,
+  `admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `country_id`, `first_name`, `last_name`, `email`, `pass`, `date_birth`, `description`, `avatar`, `hashcode`, `active`, `admin`) VALUES
+(1, 2, 'Jon', 'Snow', 'uknownothing@odesk.com', '6ce938688b031a08856e03e4679809013a4647ec', '1998-11-25 10:42:44', NULL, NULL, '0bdd627e7b8f9922caf69ee9f671982d', 0, 0),
+(2, 1, 'Admin', 'Admin', 'admin@odesk.com', '59e0513813ba536e29a8f5c456300ced38e02af7', '2017-06-11 11:07:05', NULL, NULL, '09dcb3cb36cc4ed61692ebd6f3fdb35d', 0, 1),
+(3, 1, 'Boris', 'Pire?nik', 'testing@gmail.com', 'c6f81ce1d3118d5e6e8c4ce07c19fb3c4dcf8ece', '2017-06-13 12:28:13', NULL, NULL, '', 0, 0),
+(4, 1, 'Boris', 'Pire?nik', 'boriasd@as.com', '3e863f165599a720689a8ec5717ebec72f4ce8ca', '2017-06-14 16:52:54', NULL, NULL, '', 0, 0),
+(5, 1, 'Test 2', 'Test 2', 'test@odesk.com', '6e590cf5239b0049b240c813295deacde1051f6d', '2017-06-14 16:54:34', NULL, NULL, '', 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attachments`
+--
+ALTER TABLE `attachments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `attachments_messages`
+--
+ALTER TABLE `attachments_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `message_id` (`message_id`),
+  ADD KEY `attachment_id` (`attachment_id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `project_id` (`project_id`);
+
+--
+-- Indexes for table `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userfrom_id` (`userfrom_id`),
+  ADD KEY `userto_id` (`userto_id`);
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `projects_users`
+--
+ALTER TABLE `projects_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `project_id` (`project_id`),
+  ADD KEY `role_id` (`role_id`),
+  ADD KEY `skill_id` (`skill_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `user_id_2` (`user_id`);
+
+--
+-- Indexes for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ratings_users`
+--
+ALTER TABLE `ratings_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usergiver_id` (`usergiver_id`),
+  ADD KEY `usergetr_id` (`usergetr_id`),
+  ADD KEY `rating1_id` (`rating1_id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `skills`
+--
+ALTER TABLE `skills`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `skills_users`
+--
+ALTER TABLE `skills_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `skill_id` (`skill_id`),
+  ADD KEY `rank_id` (`rank_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `country_id` (`country_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attachments`
+--
+ALTER TABLE `attachments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `attachments_messages`
+--
+ALTER TABLE `attachments_messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `projects_users`
+--
+ALTER TABLE `projects_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+--
+-- AUTO_INCREMENT for table `ratings`
+--
+ALTER TABLE `ratings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ratings_users`
+--
+ALTER TABLE `ratings_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `skills`
+--
+ALTER TABLE `skills`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `skills_users`
+--
+ALTER TABLE `skills_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attachments_messages`
+--
+ALTER TABLE `attachments_messages`
+  ADD CONSTRAINT `attachments_messages_ibfk_1` FOREIGN KEY (`message_id`) REFERENCES `messages` (`id`),
+  ADD CONSTRAINT `attachments_messages_ibfk_2` FOREIGN KEY (`attachment_id`) REFERENCES `attachments` (`id`);
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`);
+
+--
+-- Constraints for table `documents`
+--
+ALTER TABLE `documents`
+  ADD CONSTRAINT `documents_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`userfrom_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`userto_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `projects_users`
+--
+ALTER TABLE `projects_users`
+  ADD CONSTRAINT `projects_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `projects_users_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`),
+  ADD CONSTRAINT `projects_users_ibfk_3` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  ADD CONSTRAINT `projects_users_ibfk_4` FOREIGN KEY (`skill_id`) REFERENCES `skills` (`id`);
+
+--
+-- Constraints for table `ratings_users`
+--
+ALTER TABLE `ratings_users`
+  ADD CONSTRAINT `ratings_users_ibfk_1` FOREIGN KEY (`usergiver_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `ratings_users_ibfk_2` FOREIGN KEY (`usergetr_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `ratings_users_ibfk_3` FOREIGN KEY (`rating1_id`) REFERENCES `ratings` (`id`);
+
+--
+-- Constraints for table `skills`
+--
+ALTER TABLE `skills`
+  ADD CONSTRAINT `skills_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+
+--
+-- Constraints for table `skills_users`
+--
+ALTER TABLE `skills_users`
+  ADD CONSTRAINT `skills_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `skills_users_ibfk_2` FOREIGN KEY (`skill_id`) REFERENCES `skills` (`id`),
+  ADD CONSTRAINT `skills_users_ibfk_3` FOREIGN KEY (`rank_id`) REFERENCES `ranks` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
